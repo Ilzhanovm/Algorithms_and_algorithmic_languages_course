c38abcde45015140ca01b55064103580fd362e49
Magsat Ilzhanov <75102680+Ilzhanovm@users.noreply.github.com>
2021-03-25 21:54:37 +0300

    task 1.1.1 added

--- Chapter_1/task1_1_1.c
+++ Chapter_1/task1_1_1.c
@@ -4,7 +4,25 @@
 //Print to standard output the difference between the largest and smallest elements in the sequence.
 
 int task1_1_1() {
-    
+    int N;
+
+    scanf("%d", &N);
+
+    int min, max;
+    scanf("%d", &min);
+    max = min;
+
+    for (int i = 0; i < N - 1; i++) {
+        int temp;
+        scanf("%d", &temp);
+
+        if (temp > max)
+            max = temp;
+        if (temp < min)
+            min = temp;
+    }
+
+    printf("%d", max - min);
 
     return 0;
 }
