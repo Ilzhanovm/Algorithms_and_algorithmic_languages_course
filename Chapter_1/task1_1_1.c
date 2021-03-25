6505d32ed854666d3c08976450961300956ef3ad
Magsat Ilzhanov <75102680+Ilzhanovm@users.noreply.github.com>
2021-03-25 21:50:29 +0300

    Initial commit

---
+++ Chapter_1/task1_1_1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+//On standard input, a natural number N > 0 is given, followed by a sequence of N integers.
+//Print to standard output the difference between the largest and smallest elements in the sequence.
+
+int task1_1_1() {
+    
+
+    return 0;
+}
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "Chapter_1/task1_1_1.c"
+
+int main() {
+    task1_1_1();
+
+    return 0;
+}
